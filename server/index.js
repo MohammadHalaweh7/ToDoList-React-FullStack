88eb1daf336eb838ae341f3f4729fc835b491337
MohammadHalaweh7 <s11940976@stu.najah.edu>
2023-05-31 23:07:54 +0300

    Create add-todo route

--- server/index.js
+++ server/index.js
@@ -16,21 +16,34 @@ const connectToDb = () => {
   console.log("Connected To Db Successfully");
 };
 
-app.get("/get-todos", async (req, res, next) => {
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/get-todos", async (req, res) => {
   console.log("get-todos route");
 
-  const todos = await Todo.find()
+  const todos = await Todo.find();
   console.log(todos);
-  res.status(200).json({ message: "todos fetched successfully", todos: todos })
-})
-
-app.use(express.static(path.resolve(__dirname, "../client/build")));
-
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello Mohammad from server!" });
+  res.status(200).json({ message: "todos fetched successfully", todos: todos });
 });
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+
+app.post("/add-todo", async (req, res) => {
+  console.log("add-todo route");
+  const { taskname, assignee } = req.body;
+  console.log({ taskname, assignee });
+
+  const createdTodo = await Todo.create({
+    taskname: taskname,
+    assignee: assignee,
+    done: false,
+  });
+  res
+    .status(200)
+    .json({ message: "todos added successfully", todos: createdTodo });
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello Mohammad from server!" });
 });
 
 app.listen(PORT, () => {
